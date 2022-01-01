5e7989e203abb46c22147593792b7aabbdee7500
jash.liao <jash.liao@gmail.com>
2022-01-01 14:53:24 +0800

    modified 202202011453

--- chart.php
+++ chart.php
@@ -51,50 +51,38 @@
 	function showecharts(value)
 	{
 		var option;
+		
+		var SwitchData;
+		var data01 = [5, 20, 36, 10, 10, 20];
+		var data02 = [20, 10, 10, 36, 20, 5];
 		if((value%2)==1)
 		{
-			// 指定图表的配置项和数据
-			option = {
-				title: {
-					text: 'ECharts 入门示例'
-				},
-				tooltip: {},
-				legend: {
-					data:['销量']
-				},
-				xAxis: {
-					data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-				},
-				yAxis: {},
-				series: [{
-					name: '销量',
-					type: 'bar',
-					data: [5, 20, 36, 10, 10, 20]
-				}]
-			};			
+			SwitchData=data01;
 		}
 		else
 		{
-			option = {
-				title: {
-					text: 'ECharts 入门示例'
-				},
-				tooltip: {},
-				legend: {
-					data:['销量']
-				},
-				xAxis: {
-					data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-				},
-				yAxis: {},
-				series: [{
-					name: '销量',
-					type: 'bar',
-					data: [20, 10, 10, 36, 20, 5]
-				}]
-			};				
+			SwitchData=data02;
 		}
-	
+		
+		// 指定图表的配置项和数据
+		option = {
+			title: {
+				text: 'ECharts 入门示例'
+			},
+			tooltip: {},
+			legend: {
+				data:['销量']
+			},
+			xAxis: {
+				data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+			},
+			yAxis: {},
+			series: [{
+				name: '销量',
+				type: 'bar',
+				data: SwitchData
+			}]
+		};		
 		// 使用刚指定的配置项和数据显示图表。
 		myChart.setOption(option);		
 	}
